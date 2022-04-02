e42081a3e61eefbc5e86abde79511234d85371dc
Martyna Król <martyna.maria.krol@gmail.com>
2022-04-02 19:20:57 +0200

    added filter of articles by authors

--- js/script.js
+++ js/script.js
@@ -252,3 +252,79 @@ function generateAuthors(){
 }
 
 generateAuthors();
+
+function authorClickHandler(event){
+  /* prevent default action for this event */
+
+  event.preventDefault();
+
+  /* make new constant named "clickedElement" and give it the value of "this" */
+
+  const clickedElement = this;
+
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+
+  const href = clickedElement.getAttribute('href');
+
+  /* make a new constant "author" and extract author from the "href" constant */
+
+  const author = href.replace('#author-', '');
+
+  /* find all author links with class active */
+
+  const authorLinks = document.querySelectorAll('a.active[href^="#author-"]');
+
+  /* START LOOP: for each active author link */
+
+  for(let authorLink of authorLinks){
+
+    /* remove class active */
+
+    authorLink.classList.remove('active');
+
+  /* END LOOP: for each active author link */
+
+  }
+
+  /* find all author links with "href" attribute equal to the "href" constant */
+
+  const authorHrefs = document.querySelectorAll('a[href="' + href + '"]');
+
+  /* START LOOP: for each found author link */
+
+  for(let authorHref of authorHrefs){
+
+    /* add class active */
+
+    authorHref.classList.add('active');
+
+  /* END LOOP: for each found author link */
+
+  }
+
+  /* execute function "generateTitleLinks" with article selector as argument */
+
+  generateTitleLinks('[data-author="' + author + '"]');
+
+}
+
+function addClickListenersToAuthors(){
+  /* find all links to authors */
+
+  const authorLinks = document.querySelectorAll('.post-author a');
+
+  /* START LOOP: for each link */
+
+  for(let authorLink of authorLinks){
+
+    /* add authorClickHandler as event listener for that link */
+
+    authorLink.addEventListener('click', authorClickHandler);
+
+  /* END LOOP: for each link */
+
+  }
+
+}
+
+addClickListenersToAuthors();
